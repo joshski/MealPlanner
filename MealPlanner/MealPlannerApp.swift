cbab3d69cc9106f957fde0c62fa8500213367be6
Josh Chisholm <josh@featurist.co.uk>
2024-07-15 20:08:31 +0100

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Josh Chisholm on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
